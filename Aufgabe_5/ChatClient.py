c861cf28e1f30a06b7e52fb6ccf6fb8865169c42
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2022-11-19 20:00:20 +0100

    Aufgabe4 main renamed

--- Aufgabe_5/ChatClient.py
+++ Aufgabe_5/ChatClient.py
@@ -1,136 +1,104 @@
-#!/usr/bin/python3
-
-import socket
-import sys
-import re
-import traceback
-import threading
+#!/usr/bin/python
 import signal
-from functools import partial
-import os
-
-BUFFER = 2048
-
-noEnding = True
-
-
-
-def UDPBroadcast():
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        msg = 'DISCOVER'
-        sock.bind(("", 5555))
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(msg.encode("utf-8"), ("255.255.255.255", 3333))
-        addr = sock.recvfrom(1024)[0].decode()
-        print("Reply:", addr)
-        sock.close()
-    except:
-        print("Fehler1")
-        sock.close()
-        sys.exit(1)
-
-    clear = addr.split(' ')[1]
-    host = clear.split(':')[0]
-    port = int(clear.split(':')[1])
-
-    return host, port
+import socket
+import logging
+import sys
+import threading
+import curses
+import time
 
 
-def ChooseName():
-    print("Benutzername eingeben")
-    user = input("Benutzername: ")
+sock_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+msg = 'DISCOVER'
+sock_UDP.bind(("", 5555))
+sock_UDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock_UDP.sendto(msg.encode("utf-8"), ("255.255.255.255", 3333))
+addr = sock_UDP.recvfrom(1024)[0].decode()
+print("Reply:", addr)
+sock_UDP.close()
 
-    while user == "" or " " in user:
-        print("No valid name!")
-        user = input("Username: ")
+clear = addr.split(' ')[1]
+IP = clear.split(':')[0]
+PORT = int(clear.split(':')[1])
 
-    print(f"Name: {user}")
-    return user
+print('IP: ' + IP)
+print('PORT: {}'.format(PORT))
 
+name = input("Please enter chat your chatname: ")
 
-def ConnectToServer(host, port, user):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host, port))
-    except:
-        print("Error!")
-        sys.exit()
-    try:
-        connection_message = (b'CONNECT ' + user.encode() + b'\r\n')
-        sock.send(connection_message)
-    except:
-        print("Error")
-        sock.close()
-        sys.exit(1)
+sock_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_TCP.connect((IP, 3454))
+sock_TCP.send(b'CONNECT ' + name.encode("utf-8") + b'\r\n')
+print("Send chatname to Server: ", name)
+print(sock_TCP.recv(2048).decode())
 
-    return sock
+'''
+def connect(user):
 
+    sock_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_TCP.connect((IP, PORT))
+    sock_TCP.send(b'CONNECT ' + user.encode("utf-8") + b'\r\n')
+    print("Send chatname to Server: ", user)
+    print(sock_TCP.recv(2048).decode())
 
-def DisconnectFromServer(sock, user):
-    split_message = (b'DISCONNECT ' + user.encode() + b'\r\n')
-    sock.send(split_message)
-    sock.close()
+    return sock_TCP
+'''
 
+flag = True
 
+def send():
+    #temp_sock = connect(name)
+    global flag
+    while flag:
 
-def SendMessage(sock):
-    global noEnding
-    while noEnding:
-
-        neuerInput: ""
-
-        while neuerInput == "" and noEnding:
-            neuerInput = input("Eingabe: ")
-
-        neueNachricht = (b'MSG' + neuerInput.encode() + b'\r\n')
-
-        if noEnding:
-            try:
-                sock.send(neueNachricht)
-            except:
-                print("Fehler")
-                os.kill(os.getpid(), signal.SIGINT)
+        inp = input("\nSend: ")
+        if inp == "exit":
+            flag = False
+        sock_TCP.send(b'MSG ' + inp.encode("utf-8") + b'\r\n')
+        #print("Send message to Server: ", inp)
+    sock_TCP.close()
+    print("Send socket closed")
 
 
-def RecvMessage(sock):
-    global noEnding
-    while noEnding:
+def recv():
+    #temp_sock = connect(name)
+    while flag:
 
-        try:
+        data = sock_TCP.recv(2048).decode()
+        if data == "":
+            continue
+        elif data.startswith("200"):
+            continue
+        else:
+            print("\nReceive: ", data)
 
-            antwort = sock.recv(BUFFER).decode()
-            if antwort == "":
-                continue
+    sock_TCP.close()
+    print("Recv socket closed")
 
-        except:
-            print("Fehler")
+def ping():
+    sock_TCP.send(b'MSG ' + name.encode("utf-8") + b'\r\n')
 
-
-def pingServer(sock, anwender):
-    global noEnding
-    if noEnding:
-
-        neueNachricht = (b'PING ' + anwender.encode() + b'\r\n')
-
-        if noEnding:
-            try:
-                sock.send(neueNachricht)
-            except:
-                print("Fehler")
-            threading.Timer(5, pingServer, (sock, anwender,0)).start()
+def handler(signum, frame):
+    global flag
+    print("KeyboardInterrupt")
+    flag = False
+    sys.exit(0)
 
 
-host, port = UDPBroadcast()
-user = ChooseName()
-chatSocket = ConnectToServer(host, port, user)
+sendThread = threading.Thread(target=send)
+recvThread = threading.Thread(target=recv)
 
-sendThread = threading.Thread(target=SendMessage(chatSocket))
-recvThread = threading.Thread(target=RecvMessage(chatSocket))
-pingThread = threading.Thread(target=pingServer(chatSocket, user))
+signal.signal(signal.SIGINT,handler)
 
-signal.signal(signal.SIGINT, partial(chatSocket, sendThread, recvThread, pingThread, user))
+sendThread.daemon = True
+recvThread.daemon = True
 
 sendThread.start()
 recvThread.start()
-pingThread.start()
+
+sendThread.join()
+recvThread.join()
+
+
+
+
